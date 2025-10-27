85fc5c5aa95ea040b53ce899be4206208d766f5a
lukastan <lukastankovic170401@gmail.com>
2025-10-27 18:49:41 +0100

    Dodao da se upoređuju 2 izlaza, ostaje da se doda da se upoređuju 2 niza izlaza

--- code/ast.cpp
+++ code/ast.cpp
@@ -6,29 +6,35 @@
 #include <algorithm>
 
 int main() {
+    //Input
     std::string f_string;
+    std::string g_string;
     std::cout << "Rules:\n() allowed\nletters for Atoms\n~ = NOT\n& = AND\n| = OR\n" << std::endl;
-    std::cout << "Input a formula:" << std::endl;
+    std::cout << "Input 2 formulas you want to check:" << std::endl;
     std::getline(std::cin, f_string);
+    std::getline(std::cin, g_string);
 
-    Parser p(f_string);
-    FormulaPtr f = p.parse_string_into_formula();
-    std::cout << print(f) << std::endl;
+    //Parsing f
+    Parser p_f(f_string);
+    FormulaPtr f = p_f.parse_string_into_formula();
+    
+    //Parsing g
+    Parser p_g(g_string);
+    FormulaPtr g = p_g.parse_string_into_formula();
 
     //Graph showcase
-    JsonGraph g;
-    auto data = g.to_json(f);
-    std::ofstream("graph.json") << data.dump(4);
+    JsonGraph f_graph;
+    auto f_data = f_graph.to_json(f);
+    std::ofstream("f_graph.json") << f_data.dump(4);
 
-    //CNF
-    NormalForm our_cnf = cnf(f);
-    print(our_cnf);
-    std::cout << std::endl;
+    JsonGraph g_graph;
+    auto g_data = g_graph.to_json(g);
+    std::ofstream("g_graph.json") << g_data.dump(4);
 
     //P miter Q
-    FormulaPtr miter = make_miter(f, f);
+    FormulaPtr miter = make_miter(f, g);
     NormalForm miter_cnf = cnf(miter);
-    make_dimacs("test", miter_cnf);
+    make_dimacs("answer", miter_cnf);
 
     return 0;
 }
