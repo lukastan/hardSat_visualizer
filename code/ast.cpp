dcb6a62e0e23e080faf6ddaaee9fdc5cc3d6b7f6
lukastan <lukastankovic170401@gmail.com>
2025-10-27 20:22:02 +0100

    Dimacs fajlovi koje generiše cpp kod se prosleđuju minisatu na izvršavanje.

--- code/ast.cpp
+++ code/ast.cpp
@@ -4,6 +4,7 @@
 
 #include <bits/stdc++.h>
 #include <algorithm>
+#include <cstdlib>
 
 int main() {
     //Input
@@ -19,6 +20,8 @@ int main() {
 
     std::string f_string;
     std::string g_string;
+    std::string file_name = "answer";
+    std::string dimacs_file_name;
     for(int i = 0; i < n_outs; i++) {
         std::cout << "Input 2 formulas you want to check:" << std::endl;
         std::getline(std::cin, f_string);
@@ -46,8 +49,25 @@ int main() {
         //P miter Q
         FormulaPtr miter = make_miter(f, g);
         NormalForm miter_cnf = cnf(miter);
-        std::string dimacs_file_name = "answer" + std::to_string(i);
+        std::string dimacs_file_name = file_name + std::to_string(i);
         make_dimacs(dimacs_file_name, miter_cnf);
+    }
+
+    //Pipe to minisat
+    for(int j = 0; j < n_outs; j++) {
+        std::string num = std::to_string(j);
+        dimacs_file_name = file_name + num + ".cnf";
+        std::string command = "minisat " + dimacs_file_name;
+        int result = system(command.c_str());
+
+        std::string custom_msg = "";
+        switch(result/256) {
+            case 0: custom_msg = "Undefined!"; break;
+            case 10: custom_msg = "Formulas f" + num + " and g" + num + " are NOT equivalent!"; break;
+            case 20: custom_msg = "Formulas f" + num + " and g" + num + " are equivalent!"; break;
+        }
+
+        std::cout << custom_msg << std::endl << std::endl;
     }
 
     return 0;
